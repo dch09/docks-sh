d47b2b204d551d40d0164a448215649b81d2a4b7
Daniel Choroszucha <64335957+dch09@users.noreply.github.com>
2021-12-02 13:43:59 +0100

    Update docks.sh

--- docks.sh
+++ docks.sh
@@ -1,33 +1,31 @@
 presets_path=/Users/$(whoami)/Library/Preferences/DockPresets
 
 if [ -z "$1" ] || [ "$1" = "--help" ]; then
-    printf "%s\n" "Usage: dock_presets.sh command preset-name"
-    echo
     echo "Available commands:"
     echo
-    echo "save {preset-name} || saves preset with specified name"
-    echo "load {preset-name} || loads preset with specified name"
-    echo "remove {preset-name} || removes preset with specified name"
-    echo "ls || returns list of saved presets"
-    echo "open || opens presets folder in Finder"
-    echo "--help || returns usage guide"
+    echo "s, save {preset-name}    || save preset with specified name"
+    echo "l, load {preset-name}    || load preset with specified name"
+    echo "rm, remove {preset-name} || remove preset with specified name"
+    echo "ls, list                 || print list of all saved presets"
+    echo "open                     || open presets folder in Finder"
+    echo "--help                   || print out available commands"
     exit 0
 fi
 
-if [ "$1" = "save" ]; then
+if [ "$1" = "save" ] || [ "$1" = "s" ]; then
     cp -f /Users/$(whoami)/Library/Preferences/com.apple.dock.plist $presets_path/$2.plist
     echo "Dock preferenes saved."
     exit 0
 fi
 
-if [ "$1" = "load" ]; then
+if [ "$1" = "load" ] || [ "$1" = "l" ]; then
     sudo cp $presets_path/$2.plist /Users/$(whoami)/Library/Preferences/com.apple.dock.plist
     sleep 1
     killall Dock
     exit 0
 fi
 
-if [ "$1" = "ls" ]; then
+if [ "$1" = "ls" ] || [ "$1" = "list" ]; then
     echo "List of your presets:"
 
     ls -1 $presets_path | while read fname; do
@@ -35,12 +33,12 @@ if [ "$1" = "ls" ]; then
     done
 fi
 
-if [ "$1" = "remove" ]; then
+if [ "$1" = "remove" ] || [ "$1" = "rm" ]; then
     rm $presets_path/$2.plist
     echo $2 removed.
 fi
 
-if [ "$1" = "open" ]; then
+if [ "$1" = "open" ] || [ "$1" = "." ]; then
     cd $presets_path
     open .
     echo "Folder opened in Finder"
